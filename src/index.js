d148e141840bf9fc5d9e9f641c680d6f99e31a40
isbnn <isb.cv@yandex.ru>
2019-09-25 00:06:22 +0300

    brackets/feat: complete task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,33 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+    const [openAlphabet, closeAlphabet] = bracketsConfig.reduce(
+        (acc, el) => {
+            const [openAlphabet, closeAlphabet] = acc;
+            return [
+                { ...openAlphabet, [el[0]]: el[1] },
+                `${closeAlphabet}${el[1]}`
+            ];
+        },
+        [{}, ""]
+    );
+
+    const isCanDel = (str, sym) => str.length && str[0] === sym;
+
+    const resultString = [...str].reduce((acc, el) => {
+        console.log("::");
+        console.log("acc:", acc);
+        console.log("el:", el);
+
+        if (
+            Object.keys(openAlphabet).includes(el) &&
+            closeAlphabet.indexOf(el) !== -1
+        ) {
+            return isCanDel(acc, el) ? acc.slice(1) : [el, ...acc];
+        } else if (Object.keys(openAlphabet).includes(el)) {
+            return [openAlphabet[el], ...acc];
+        } else {
+            return isCanDel(acc, el) ? acc.slice(1) : ["ERROR"];
+        }
+    }, []);
+
+    return !resultString.length;
+};
